8d4811f1c5421ff4d412544f1728540e643ff761
parfaitBashombe <parfaitbashombe@gmail.com>
2026-03-08 14:44:34 +0300

    ft(auth):added

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,30 @@ import Auth from "./pages/Auth";
 import Account from "./pages/Account";
 import Navbar from "./components/navbar";
 
+import { NeonAuthUIProvider } from "@neondatabase/neon-js/auth/react";
+import { authClient } from "./lib/auth";
+import AuthProvider from "./context/auth-provider";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/auth/:pathname" element={<Auth />} />
-            <Route path="/account/:pathname" element={<Account />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <NeonAuthUIProvider authClient={authClient} defaultTheme="dark">
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="flex-1">
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/onboarding" element={<Onboarding />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/auth/:pathname" element={<Auth />} />
+                <Route path="/account/:pathname" element={<Account />} />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </NeonAuthUIProvider>
   );
 };
 
